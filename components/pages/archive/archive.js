335cfb0cb82b6841a8cafc97d43bc5acbde1e20a
Gustavo Miranda <gustavohssmiranda@gmail.com>
2022-10-12 17:22:20 -0300

    Adicionado o Drawer Navigation e 3 páginas necessárias

---
+++ components/pages/archive/archive.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export const ArchiveScreen = ({ navigation,routes}) => {
+    return(
+        <Text>Pagina de Arquivamento</Text>
+    )
+}
